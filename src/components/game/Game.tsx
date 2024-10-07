900b9cb6f47dad10634e77921ff4e5bd6d3734b6
Виктор <vityan99@MacBook-Pro-Viktor.local>
2024-10-07 19:38:11 +0300

    realized pairs init

--- src/components/game/Game.tsx
+++ src/components/game/Game.tsx
@@ -1,10 +1,7 @@
-import { FC, useState } from "react";
-
+import { FC, useEffect, useState } from "react";
 import style from "./game.module.css";
 import cn from "classnames";
-
 import Button from "../button/Button";
-
 import type { DataType } from "../../assets/data/data";
 
 type DataGameType = {
@@ -13,10 +10,26 @@ type DataGameType = {
 
 const Game: FC<DataGameType> = ({ data }): React.ReactElement => {
   const [gameStart, setGameStart] = useState<boolean>(false);
-  const [gameValues, setGameValues] = useState([]);
-
+  const [pairs, setPairs] = useState<number>(0);
   const [total, setTotal] = useState<number>(0);
-  const [found, setFound] = useState<number>(0);
+
+  useEffect(() => {
+    const findPairs = (data: DataType[]): number => {
+      const countMap: Record<string, number> = {};
+
+      data.forEach(({ color }) => {
+        countMap[color] = (countMap[color] || 0) + 1;
+      });
+
+      return Object.values(countMap).reduce((acc, count) => {
+        return acc + Math.floor(count / 2);
+      }, 0);
+    };
+
+    const calculatedPairs = findPairs(data);
+    setPairs(calculatedPairs);
+    setTotal(data.length);
+  }, [data]);
 
   const gameStartHandler = () => setGameStart((current) => !current);
 
@@ -27,14 +40,16 @@ const Game: FC<DataGameType> = ({ data }): React.ReactElement => {
       </div>
 
       <div className={cn(style.game__info)}>
-        <span className={cn(style["game__info-note"])}>Найдено: {found}</span>
+        <span className={cn(style["game__info-note"])}>Найдено пар: {pairs}</span>
         <span className={cn(style["game__info-note"])}>Всего: {total}</span>
       </div>
 
       <div className={cn(style.game__area)}>
-        {/* <Button use="transparent" size="large" clickHandler={gameStartHandler}>
-          <img src={stone_1} alt="asdasdas" />
-        </Button> */}
+        {data.map(({ id, color, img }) => (
+          <Button use="transparent" size="large" key={id}>
+            <img src={img} alt={color} />
+          </Button>
+        ))}
       </div>
     </div>
   );
